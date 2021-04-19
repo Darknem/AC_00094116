3882f79aefb9b4cae86aa22d0bc528c6aabc9cfa
Nelson Mossa <54344643+Darknem@users.noreply.github.com>
2021-04-19 17:58:52 -0600

    Laboratorio 2 terminado

---
+++ Tarea Laboratorio 2/tarea.asm
@@ -0,0 +1,29 @@
+        org     100h
+
+        section .text
+
+        mov     AL, "N"
+        mov     BL, "M"
+        mov     CL, "V"
+        mov     DL, "M"
+
+        mov     [200h], AL
+        mov     [201h], BL
+        mov     [202h], CL
+        mov     [203h], DL
+
+        ;Direccionamiento directo
+        mov     AX, [200h]
+
+        ;Direccionamiento indirecto por registro
+        mov     BX, 201h
+        mov     CX, [BX]
+
+        ;Direccionamiento indirecto base + índice
+        mov     DX, [BX+SI]
+
+        ;Direccionamiento relativo por registro
+        mov     DI, [SI+203h]
+
+
+        int     20h
