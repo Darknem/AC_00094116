84c990a70ce450635a7202b68d737bc736899cd6
Nelson Mossa <54344643+Darknem@users.noreply.github.com>
2021-05-12 02:50:17 -0600

    Ejercicio 2 terminado

---
+++ Tarea Laboratorio 3/Ejercicio2.asm
@@ -0,0 +1,14 @@
+;Factorial de un número
+
+        org     100h
+
+        section .text
+
+        mov     CX, 5
+        mov     AX, 1
+
+condic  mul     CX
+        loop    condic
+
+
+        int     20h
